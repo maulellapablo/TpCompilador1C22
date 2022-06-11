cdd61dd103e1c4469a332453d5ce0cb5e0d5473f
Lautaro Carlos D'Ercole <ldercole@alumno.unlam.edu.ar>
2022-06-11 00:40:49 -0300

    Fix replace

--- segunda_entrega/arbol.h
+++ segunda_entrega/arbol.h
@@ -56,7 +56,7 @@ void inOrder(t_arbol *pa, FILE *pIntermedia)
 void grabarNodoDOT(t_nodo *pn, FILE* stream, int* numero)
 {
     int thisId = (*numero);
-    fprintf(stream, "id%d [label = \"%s\"];\n", thisId, str_replace((*pn).data,'"','\"'));
+    fprintf(stream, "id%d [label = \"%s\"];\n", thisId, replace_char((*pn).data,'"','\''));
 
     if ((*pn).izq)
     {
@@ -92,51 +92,13 @@ void generarDOT(t_arbol* pa, FILE* stream)
 
 
 // Función de ayuda, replace string
-// You must free the result if result is non-NULL.
-char *str_replace(char *orig, char *rep, char *with) {
-    char *result; // the return string
-    char *ins;    // the next insert point
-    char *tmp;    // varies
-    int len_rep;  // length of rep (the string to remove)
-    int len_with; // length of with (the string to replace rep with)
-    int len_front; // distance between rep and end of last rep
-    int count;    // number of replacements
-
-    // sanity checks and initialization
-    if (!orig || !rep)
-        return NULL;
-    len_rep = strlen(rep);
-    if (len_rep == 0)
-        return NULL; // empty rep causes infinite loop during count
-    if (!with)
-        with = "";
-    len_with = strlen(with);
-
-    // count the number of replacements needed
-    ins = orig;
-    for (count = 0; tmp = strstr(ins, rep); ++count) {
-        ins = tmp + len_rep;
+char* replace_char(char* str, char find, char replace){
+    char *current_pos = strchr(str,find);
+    while (current_pos) {
+        *current_pos = replace;
+        current_pos = strchr(current_pos,find);
     }
-
-    tmp = result = malloc(strlen(orig) + (len_with - len_rep) * count + 1);
-
-    if (!result)
-        return NULL;
-
-    // first time through the loop, all the variable are set correctly
-    // from here on,
-    //    tmp points to the end of the result string
-    //    ins points to the next occurrence of rep in orig
-    //    orig points to the remainder of orig after "end of rep"
-    while (count--) {
-        ins = strstr(orig, rep);
-        len_front = ins - orig;
-        tmp = strncpy(tmp, orig, len_front) + len_front;
-        tmp = strcpy(tmp, with) + len_with;
-        orig += len_front + len_rep; // move to next "end of rep"
-    }
-    strcpy(tmp, orig);
-    return result;
+    return str;
 }
 
 #endif // ARBOL_H_INCLUDED
